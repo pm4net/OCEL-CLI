d50200a41520b4050f13912a46931f4ad77e856f
Johannes Mols <johannes.mols@kraftvaerk.com>
2022-12-20 13:53:21 +0100

    Implemented convert & merge directory

--- CLI/Program.fs
+++ CLI/Program.fs
@@ -70,54 +70,59 @@ and Args =
             | ConvertMergeFiles _ -> "Convert and merge one or more OCEL files into a single file."
 
 /// Get OCEL files in a directory (throws exception if directory not found)
-let private getOcelFilesInDir dir =
+let private ocelFilesInDir dir =
     dir
     |> Directory.GetFiles 
     |> Array.toList 
     |> List.filter (fun f -> 
+        f.EndsWith ".json" ||
         f.EndsWith ".jsonocel" ||
+        f.EndsWith ".xml" ||
         f.EndsWith ".xmlocel" ||
         f.EndsWith ".db" ||
         f.EndsWith ".litedb"
     )
 
 /// Get the file extension of an OCEL format
-let private getFileExtension format =
+let private fileExtensionForFormat format =
     match format with
     | OcelFormat.Json -> ".jsonocel"
     | OcelFormat.Xml -> ".xmlocel"
     | OcelFormat.LiteDb -> ".db"
     | _ -> raise (ArgumentOutOfRangeException(nameof(format)))
 
-/// Filter a list of file paths based on an OCEL format
-let private filterByFormat format (filePaths: string list) =
-    filePaths |> List.filter (fun f ->
-        match format with
-        | OcelFormat.Json -> getFileExtension OcelFormat.Json |> f.EndsWith
-        | OcelFormat.Xml -> getFileExtension OcelFormat.Xml |> f.EndsWith
-        | OcelFormat.LiteDb -> getFileExtension OcelFormat.LiteDb |> f.EndsWith || f.EndsWith ".litedb"
-        | _ -> raise (new ArgumentOutOfRangeException(nameof(format)))
-    )
-
 /// Find all files with the specified OCEL format in a directory
 let private findFilesByFormat dir format =
     match format with
-    | Some format -> getOcelFilesInDir dir |> filterByFormat format
-    | None -> getOcelFilesInDir dir
+    | Some format -> 
+        ocelFilesInDir dir 
+        |> List.filter (fun f ->
+            match format with
+            | OcelFormat.Json -> f.EndsWith ".json" || f.EndsWith ".jsonocel"
+            | OcelFormat.Xml -> f.EndsWith ".xml" || f.EndsWith ".xmlocel"
+            | OcelFormat.LiteDb -> f.EndsWith ".db" || f.EndsWith ".litedb"
+            | _ -> raise (new ArgumentOutOfRangeException(nameof(format)))
+        )
+    | None -> ocelFilesInDir dir
 
 /// Generate the new file name of an input file, considering its output directory and format
 let private getNewFileName (inputFile: string) (outDir: string) format =
     let fileName = inputFile |> Path.GetFileNameWithoutExtension
-    let fileFullName = fileName + getFileExtension format
+    let fileFullName = fileName + fileExtensionForFormat format
     Path.Combine(outDir, fileFullName)
 
 /// Read an OCEL file into a log object
 let private readOcelFile (path: string) =
-    match path with
-    | p when p.EndsWith ".jsonocel" -> path |> File.ReadAllText |> OCEL.OcelJson.deserialize
-    | p when p.EndsWith ".xmlocel" -> path |> File.ReadAllText |> OCEL.OcelXml.deserialize
-    | p when p.EndsWith ".db" || p.EndsWith ".litedb" -> new LiteDatabase $"Filename={p};ReadOnly=true;" |> OCEL.OcelLiteDB.deserialize
-    | _ -> failwith "File is not of any of the supported formats (.jsonocel, .xmlocel, .db, .litedb)."
+    try
+        match path with
+        | p when p.EndsWith ".json" || p.EndsWith ".jsonocel" -> path |> File.ReadAllText |> OCEL.OcelJson.deserialize |> Some
+        | p when p.EndsWith ".xml" || p.EndsWith ".xmlocel" -> path |> File.ReadAllText |> OCEL.OcelXml.deserialize |> Some
+        | p when p.EndsWith ".db" || p.EndsWith ".litedb" -> new LiteDatabase $"Filename={p};ReadOnly=true;" |> OCEL.OcelLiteDB.deserialize |> Some
+        | _ -> failwith "File is not of any of the supported formats (.jsonocel, .xmlocel, .db, .litedb)."
+    with
+    | e -> 
+        printfn $"Failed to read or deserialize file {path}: {e.Message}."
+        None
 
 [<EntryPoint>]
 let main args =
@@ -148,25 +153,57 @@ let main args =
                 files
             |> List.map (fun f -> f, readOcelFile f)
             |> List.iter (fun (name, log) ->
-                match outputFormat with
-                | OcelFormat.Json ->
-                    let fileName = getNewFileName name outDir outputFormat
-                    let json = OCEL.OcelJson.serialize formatting log
-                    printfn $"Writing log to {fileName}."
-                    File.WriteAllText(fileName, json)
-                | OcelFormat.Xml ->
-                    let fileName = getNewFileName name outDir outputFormat
-                    let xml = OCEL.OcelXml.serialize formatting log
-                    printfn $"Writing log to {fileName}."
-                    File.WriteAllText(fileName, xml)
-                | OcelFormat.LiteDb ->
-                    let fileName = getNewFileName name outDir outputFormat
-                    let outDb = new LiteDatabase(fileName)
-                    printfn $"Writing log to {fileName}."
-                    OCEL.OcelLiteDB.serialize outDb log
-                | _ -> raise (new ArgumentOutOfRangeException(nameof(outputFormat)))
+                match log with
+                | Some log ->
+                    match outputFormat with
+                    | OcelFormat.Json ->
+                        let fileName = getNewFileName name outDir outputFormat
+                        let json = OCEL.OcelJson.serialize formatting log
+                        printfn $"Writing log to {fileName}."
+                        File.WriteAllText(fileName, json)
+                    | OcelFormat.Xml ->
+                        let fileName = getNewFileName name outDir outputFormat
+                        let xml = OCEL.OcelXml.serialize formatting log
+                        printfn $"Writing log to {fileName}."
+                        File.WriteAllText(fileName, xml)
+                    | OcelFormat.LiteDb ->
+                        let fileName = getNewFileName name outDir outputFormat
+                        let outDb = new LiteDatabase(fileName)
+                        printfn $"Writing log to {fileName}."
+                        OCEL.OcelLiteDB.serialize outDb log
+                    | _ -> raise (new ArgumentOutOfRangeException(nameof(outputFormat)))
+                | None -> ()
             )
-        | None, Some cmd, None, None -> raise (NotImplementedException())
+        | None, Some cmd, None, None ->
+            let dir = cmd.GetResult ConvertMergeDirArgs.Dir
+            let out = cmd.GetResult ConvertMergeDirArgs.Out
+
+            let mergedLog =
+                findFilesByFormat dir inputFormat
+                |> fun files ->
+                    printfn $"Found {files.Length} matching files in directory."
+                    files
+                |> List.map (fun f -> readOcelFile f)
+                |> List.choose id
+                |> fun files ->
+                    printfn $"Merging {files.Length} files into one."
+                    files
+                |> List.fold (fun (state: OCEL.Types.OcelLog) log -> state.MergeWith log) OCEL.Types.OcelLog.Empty
+
+            match outputFormat with
+            | OcelFormat.Json ->
+                let json = OCEL.OcelJson.serialize formatting mergedLog
+                printfn $"Writing log to {out}."
+                File.WriteAllText(out, json)
+            | OcelFormat.Xml ->
+                let xml = OCEL.OcelXml.serialize formatting mergedLog
+                printfn $"Writing log to {out}."
+                File.WriteAllText(out, xml)
+            | OcelFormat.LiteDb ->
+                let outDb = new LiteDatabase(out)
+                printfn $"Writing log to {out}."
+                OCEL.OcelLiteDB.serialize outDb mergedLog
+            | _ -> raise (new ArgumentOutOfRangeException(nameof(outputFormat)))
         | None, None, Some cf, None -> raise (NotImplementedException())
         | None, None, None, Some cmf -> raise (NotImplementedException())
         | _ -> failwith "Only one sub-command allowed at a time."
